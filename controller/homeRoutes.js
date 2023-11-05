ce42dcfbb675d00baa0e37be2ea250fbcdb8ed7c
MAT-2 <mpcasey30@gmail.com>
2023-11-04 19:53:21 -0500

    Added a basic GET route to test rendering handlebar.js files. Added notes for explanation.

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,35 +1,41 @@
-const router =require('express').Router();
-const { AAAA, BBBB} = require('../models');
+const router = require("express").Router();
+const { AAAA, BBBB } = require("../models");
 
-router.get('/', async (req,res) =>{
-    try{
-        const stressData = await AAAA.findAll({
-            include: [
-                {
-                    model: BBBB,
-                    attributes: ['name'],
-                },
-            ],
-        });
-        
-        const stress = stressData.get({ plain: true });
-
-        res.render('stress', {
-            ...stress,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+//GET Route to get information from homepage.handlebars
+router.get("/", async (req, res) => {
+  // Renders all Handlebars.js template.
+  res.render("all");
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/stressQuestions');
-        return;
-    }
+router.get("/", async (req, res) => {
+  try {
+    const stressData = await AAAA.findAll({
+      include: [
+        {
+          model: BBBB,
+          attributes: ["name"],
+        },
+      ],
+    });
 
-    res.render('login');
+    const stress = stressData.get({ plain: true });
+
+    res.render("stress", {
+      ...stress,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/stressQuestions");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
