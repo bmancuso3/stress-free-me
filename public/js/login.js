d6e999519df25d979e079b8e5bca740d89c122e5
MAT-2 <mpcasey30@gmail.com>
2023-11-06 13:08:30 -0600

    Added beginning for collecting login details, using async await functionality that will be used, and const variables for query selecting email and password. Added beginning if statment when login and password are both true.

---
+++ public/js/login.js
@@ -0,0 +1,10 @@
+const loginForm = async (event) => {
+  event.preventDefault();
+
+  //Grabbing the values from our homepage.handlebars
+  const email = document.querySelector("").value.trim();
+  const password = document.querySelector("").value.trim();
+
+  if (email && password === true) {
+  }
+};
