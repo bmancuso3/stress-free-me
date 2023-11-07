73c6926282d4155bad10b552f50edb97a39e3f54
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-11-07 13:33:46 -0600

    submit buttons takes you to result page

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const surveyRoutes = require('./surveyRoutes');
-const resultRoutes = require('./resultRoutes');
+
 
 router.use('/users', userRoutes);
 router.use('/surveys', surveyRoutes);
-router.use('/results', resultRoutes);
+
 
 module.exports = router;
