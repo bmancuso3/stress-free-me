1667115ba804057cd4b5d9bca51dee6faea8c4a4
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-11-07 15:48:24 -0600

    selected value is stored on the database

--- public/js/survey.js
+++ public/js/survey.js
@@ -2,34 +2,35 @@ const submitButton = document.querySelector('.btn-primary');
 
 submitButton.addEventListener('click', async () => {
   const radioButtons = document.querySelectorAll('input[name="exampleForm"]:checked');
+const data = {
+  sleep_quality: radioButtons[0].value, 
+  headaches: radioButtons[1].value,
+  performance: radioButtons[2].value,
+  workload: radioButtons[3].value,
+  hobbies: radioButtons[4].value,
+  stress: radioButtons[5].value,
+  therapy: radioButtons[6].value,
+  outside: radioButtons[7].value,
+}
 
-  // Iterate over each selected radio button
-  for (const radioButton of radioButtons) {
-    const rating = radioButton.value;
+  console.log(data);
+  try {
+    // Make the fetch POST request
+    const response = await fetch('/api/surveys', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
 
-    // Create an object with the data to be sent in the request body. Wouldn't this need more than one property on the req.body?
-    const data = {
-      rating: rating
-    };
-console.log(data)
-    try {
-      // Make the fetch POST request
-      const response = await fetch('/api/surveys', { 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-
-      const responseData = await response.json();
-      // Handle the response data
-      console.log(responseData);
-    } catch (error) {
-      // Handle any errors
-      console.error(error);
-    }
+    const responseData = await response.json();
+    // Handle the response data
+    console.log(responseData);
+  } catch (error) {
+    // Handle any errors
+    console.error(error);
   }
-  document.location.replace("/api/results"); //goes to result page.
+  document.location.replace("/api/profile/:id"); //goes to homepage OR once profile is set. This will go to profile.
 });
 
