178ede3585d98839a5ecf17c0eab2ae401383c0d
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-11-08 12:50:18 -0600

    all the links are linked up

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,4 +1,5 @@
 const logout = async () => {
+    console.log(logout)
     const response = await fetch('/api/users/logout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
